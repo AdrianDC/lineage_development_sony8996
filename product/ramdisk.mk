8c71964113e7b85392a0b9d5f1c64070e0c98fac
Adrian DC <radian.dc@gmail.com>
2017-02-12 00:45:25 +0100

    dora: ramdisk: Initial rootdir base files
     * Empty init.qcom.rc, init.recovery.qcom.rc files
     * Initial init.device.rc used to wrap init.qcom.rc
     * Build the init files as prebuilts
    
    Change-Id: I91f878712461ff5053b618da40aac36b8a2f7d7e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ product/ramdisk.mk
@@ -0,0 +1,6 @@
+# Ramdisk packages
+PRODUCT_PACKAGES += \
+    init.dora.rc \
+    init.qcom.rc \
+    init.recovery.qcom.rc \
+    ueventd.dora.rc
---
+++ rootdir/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH:= $(call my-dir)
+
+# Init configurations
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.$(TARGET_DEVICE).rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.$(TARGET_DEVICE).rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)

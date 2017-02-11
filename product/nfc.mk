48db6eba7e1d8151783f832668723dcc6afeb8b4
Adrian DC <radian.dc@gmail.com>
2017-02-12 00:51:37 +0100

    dora: nfc: Initial hardware configurations
     * Configurations extracted from official Nougat 39.2.A.0.361
     * Define the pn547 chipset
     * Include the related dependencies
     * Include frameworks permissions
     * Add related product packages
    
    Change-Id: Ia7be3c586f6bdf8cfabd9b201f5fe51fd3cf4c90

---
+++ product/nfc.mk
@@ -0,0 +1,4 @@
+# NFC configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
---
+++ product/qcom-nfc.mk
@@ -0,0 +1,12 @@
+# NFC permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    nfc_nci.pn54x.default \
+    Tag

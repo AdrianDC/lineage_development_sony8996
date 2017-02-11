9c0f5185e5fc445be9b64b8958d3a63ee7e8ae0f
Adrian DC <radian.dc@gmail.com>
2017-02-12 00:57:56 +0100

    dora: kernel: Initial zImage build configurations
     * Define proper kernel properties matching Sony SODP / tone
     * Declare official Nougat cmdline
     * Declare DTB has to be appended
     * Define zImage output name
    
    Change-Id: I7ba98949582b69677559ca210179019907bead82

---
+++ board/kernel.mk
@@ -0,0 +1,27 @@
+# Kernel informations
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+# Kernel cmdline
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7464900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.hardware=dora
+BOARD_KERNEL_CMDLINE += user_debug=31
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
+BOARD_KERNEL_CMDLINE += coherent_pool=8M
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1
+
+# Kernel properties
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+
+# Kernel configuration
+TARGET_KERNEL_CONFIG := tone_dora_defconfig
+TARGET_KERNEL_SOURCE := kernel/sony/msm8996

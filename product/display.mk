c2844d8ad67286be8c969724e89acd1fe61f1923
Adrian DC <radian.dc@gmail.com>
2017-02-12 00:55:17 +0100

    dora: display: Initial configurations and packages
     * Configure display board configurations for msm8996
     * Replicate display properties from official Nougat
     * Build the relevant graphics packages
     * Include frameworks permissions
     * Define the OpenGLES version
    
    Change-Id: I5ddd50a4a9ce782f8a442f3d4fd84acdab13e4f8

---
+++ product/display.mk
@@ -0,0 +1,6 @@
+# Display properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.hwc.mdpcomp.enable=true \
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=480 \
+    sdm.perf_hint_window=50
---
+++ product/qcom-display.mk
@@ -0,0 +1,17 @@
+# Graphics permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
+
+# Display QCOM packages
+PRODUCT_PACKAGES += \
+    copybit.msm8996 \
+    gralloc.msm8996 \
+    hdmi_cec.msm8996 \
+    hwcomposer.msm8996 \
+    memtrack.msm8996
+
+# OpenGL properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610

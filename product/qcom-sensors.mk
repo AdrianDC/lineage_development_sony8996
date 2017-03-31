5623c4ed09a06f1c68baa390b5c0ca1786b0aeab
Adrian DC <radian.dc@gmail.com>
2017-04-01 00:49:30 +0200

    dora: sensors: Add HAL sensors.msm8996 product compilation
    Change-Id: I3902f105874231694f5823a832f5eae05afd1aec

--- product/qcom-sensors.mk
+++ product/qcom-sensors.mk
@@ -9,3 +9,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
+
+# Sensors packages
+PRODUCT_PACKAGES += \
+    sensors.msm8996
